3ec92f612ba2ac287fd785a4c2e694aa12b3b5f9
Dmitry Moiseev <mdmitry94@gmail.com>
2022-09-17 13:21:19 +0300

    feat: add schema for post request

--- src/features/timer/services/time-entries.ts
+++ src/features/timer/services/time-entries.ts
@@ -214,3 +214,51 @@ export async function getTimeEntries(
     timeEntriesSchema,
   )
 }
+
+const createdTimeEntrySchema = z.object({
+  billable: z.boolean(),
+  description: z.string(),
+  id: z.string(),
+  isLocked: z.boolean(),
+  projectId: z.string(),
+  tagIds: z.array(z.string()).nullable(),
+  taskId: z.string().nullable(),
+  timeInterval: activeTimeEntryIntervalSchema,
+  userId: z.string(),
+  workspaceId: z.string(),
+  customFieldValues: z
+    .array(
+      z.object({
+        customFieldId: z.string(),
+        timeEntryId: z.string(),
+        value: z.string(),
+        name: z.string(),
+        type: z.string(),
+      }),
+    )
+    .nullable(),
+})
+
+export type CreatedTimeEntry = z.infer<typeof createdTimeEntrySchema>
+
+export type CreateTimeEntryPayload = {
+  start: string
+  billable: boolean
+  description: string
+  projectId: string
+  taskId: string
+  end: string
+  tagIds: string[]
+  customFields: { customFieldId: string; value: string }[]
+}
+
+export async function createTimeEntry(
+  workspaceId: string,
+  data: CreateTimeEntryPayload,
+) {
+  return client<CreatedTimeEntry>(
+    `/workspaces/${workspaceId}/time-entries`,
+    createdTimeEntrySchema,
+    { data },
+  )
+}
