d9d665675dc57521057fb22182a7c5b9bdef02f2
Dmitry Moiseev <mdmitry94@gmail.com>
2022-10-09 15:00:05 +0300

    feat: add new components

---
+++ src/common/components/List.tsx
@@ -0,0 +1,12 @@
+import { Card } from 'common/components/Card';
+import { styled } from 'core/theme/config';
+
+export const List = styled(Card, {
+  display: 'flex',
+  flexDirection: 'column',
+});
+
+export const ListItem = styled('div', {
+  padding: '$6 $8',
+  borderBottom: '1px solid $neutral2',
+});
