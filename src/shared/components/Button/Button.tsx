6dff1cc0df34a2d780687b89900287532e6c9a4b
Dmitry Moiseev <mdmitry94@gmail.com>
2022-11-02 21:15:56 +0200

    refactor: tailwind refactoring

--- src/shared/components/Button/Button.tsx
+++ src/shared/components/Button/Button.tsx
@@ -61,8 +61,10 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
           ],
           sizes[size],
           variants[variant],
-          disabled &&
-            'cursor-not-allowed bg-neutral-300 hover:bg-neutral-300 dark:bg-neutral-dark-300 dark:hover:bg-neutral-dark-300',
+          disabled && [
+            'dark:bg-neutral-dark-30 cursor-not-allowed  bg-neutral-300',
+            'hover:bg-neutral-300 dark:hover:bg-neutral-dark-300',
+          ],
           className,
         )}
         disabled={disabled}
