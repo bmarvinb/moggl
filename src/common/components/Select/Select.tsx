70fd9ba140f1e6c8fb909948fdaf3c8d6de6faab
Dmitry Moiseev <mdmitry94@gmail.com>
2022-11-12 12:43:02 +0200

    feat: basic select

--- src/common/components/Select/Select.tsx
+++ src/common/components/Select/Select.tsx
@@ -1,68 +1,112 @@
 import { Listbox, Transition } from '@headlessui/react';
-import { Fragment, useState } from 'react';
+import clsx from 'clsx';
 import { BiCheck, BiChevronDown } from 'react-icons/bi';
 
-const people = [
-  { name: 'Wade Cooper' },
-  { name: 'Arlene Mccoy' },
-  { name: 'Devon Webb' },
-  { name: 'Tom Cook' },
-  { name: 'Tanya Fox' },
-  { name: 'Hellen Schmidt' },
-];
+type SelectValue = string | number | undefined;
 
-export function Select() {
-  const [selected, setSelected] = useState(people[0]);
+type SelectOption = {
+  id: string;
+  value: SelectValue;
+  label: string;
+};
+
+type SelectProps = {
+  name: string;
+  value: SelectValue;
+  options: SelectOption[];
+  placeholder?: string;
+  invalid?: boolean;
+  disabled?: boolean;
+  className?: string;
+  onChange: (value: SelectValue) => void;
+};
+
+export type SelectOptions = SelectOption[];
+
+export function Select({
+  name,
+  value,
+  options,
+  disabled,
+  invalid,
+  className,
+  onChange,
+  placeholder = 'Select',
+}: SelectProps) {
+  const label = options.find(option => option.value === value)?.label;
+  console.log('label', label);
 
   return (
-    <Listbox value={selected} onChange={setSelected}>
+    <Listbox
+      name={name}
+      value={value}
+      onChange={onChange}
+      disabled={disabled}
+      multiple={false}
+    >
       <div className="relative">
-        <Listbox.Button className="relative w-full cursor-default rounded  border border-neutral-50 bg-neutral-300 p-2 py-2 pl-3 pr-10 text-left text-neutral-900 placeholder-neutral-700 shadow-md focus:border-primary-400 focus:outline-none focus:ring-2 focus:ring-primary-600 focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-neutral-50 dark:border-neutral-dark-300 dark:bg-neutral-dark-400 dark:text-neutral-dark-900 dark:placeholder-neutral-dark-700 dark:focus:ring-primary-dark-300 dark:focus-visible:ring-offset-neutral-dark-50">
-          <span className="block truncate">{selected.name}</span>
+        <Listbox.Button
+          value={value}
+          className={clsx(
+            'block w-full min-w-0 rounded border bg-neutral-50 p-2 text-left focus:outline-none focus:ring-2 dark:bg-neutral-dark-400',
+            label !== undefined
+              ? 'text-neutral-900 dark:text-neutral-dark-900'
+              : 'text-neutral-600 dark:text-neutral-dark-600',
+            invalid
+              ? 'border-red-400 focus:ring-red-300 dark:border-red-dark-400 dark:focus:ring-red-dark-400'
+              : 'border-neutral-200 focus:border-primary-400 focus:ring-primary-300 dark:border-neutral-dark-300 dark:focus:border-primary-dark-400 dark:focus:ring-primary-dark-300',
+            disabled &&
+              'dark:placeholder-dark-600 bg-neutral-300/25 placeholder-neutral-600 dark:bg-neutral-dark-300/25',
+            className,
+          )}
+        >
+          <span className="block truncate">{label || placeholder}</span>
           <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
             <BiChevronDown
-              className="h-5 w-5 text-gray-400"
+              className="dark:text-gray-dark-400 h-5 w-5 text-gray-400"
               aria-hidden="true"
             />
           </span>
         </Listbox.Button>
+
         <Transition
-          as={Fragment}
           leave="transition ease-in duration-100"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-neutral-900 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-neutral-dark-400 sm:text-sm">
-            {people.map((person, personIdx) => (
-              <Listbox.Option
-                key={personIdx}
-                className={({ active }) =>
-                  `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                    active
-                      ? 'bg-primary-500 text-primary-900 dark:bg-primary-dark-500 dark:text-primary-dark-900'
-                      : 'text-neutral-900 dark:text-neutral-dark-900'
-                  }`
-                }
-                value={person}
-              >
-                {({ selected }) => (
-                  <>
-                    <span
-                      className={`block truncate ${
-                        selected ? 'font-medium' : 'font-normal'
-                      }`}
-                    >
-                      {person.name}
-                    </span>
-                    {selected ? (
-                      <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-neutral-900 dark:text-neutral-dark-900">
-                        <BiCheck className="h-5 w-5" aria-hidden="true" />
+          <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-neutral-50 py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-neutral-dark-400 sm:text-sm">
+            {options.map(option => {
+              return (
+                <Listbox.Option
+                  key={option.id}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active
+                        ? 'bg-primary-400 text-neutral-50 dark:bg-primary-dark-400 dark:text-neutral-dark-900'
+                        : 'text-neutral-900 dark:text-neutral-dark-900'
+                    }`
+                  }
+                  value={option.value}
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {option.label}
                       </span>
-                    ) : null}
-                  </>
-                )}
-              </Listbox.Option>
-            ))}
+                      {selected ? (
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-neutral-900 dark:text-neutral-dark-900">
+                          <BiCheck className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Listbox.Option>
+              );
+            })}
           </Listbox.Options>
         </Transition>
       </div>
--- src/features/tags/components/TagsFilter.tsx
+++ src/features/tags/components/TagsFilter.tsx
@@ -1,6 +1,6 @@
 import { Card } from 'common/components/Card/Card';
 import { Input } from 'common/components/Form/Input';
-import { Select } from 'common/components/Select';
+import { Select, SelectOptions } from 'common/components/Select';
 import { TagsSearchFilterCriteria } from '../hooks/useTagSearchCriteria';
 
 type TagsFilterProps = {
@@ -8,25 +8,38 @@ type TagsFilterProps = {
   onFilterChange: (values: TagsSearchFilterCriteria) => void;
 };
 
+const options: SelectOptions = [
+  {
+    id: 'archived',
+    value: 'true',
+    label: 'Show archived',
+  },
+  {
+    id: 'active',
+    value: 'false',
+    label: 'Show active',
+  },
+];
+
 export const TagsFilter = ({ criteria, onFilterChange }: TagsFilterProps) => {
   return (
     <Card className="mb-6">
       <form onSubmit={e => e.preventDefault()}>
         <div className="flex gap-4 px-6 py-8">
-          <Select />
-          {/* <select
+          <div className="w-[10rem]">
+            <Select
               name="archived"
               value={criteria.archived}
-              onChange={e =>
+              options={options}
+              onChange={value =>
                 onFilterChange({
                   ...criteria,
-                  archived: e.target.value === 'true' ? 'true' : 'false',
+                  archived: value === 'true' ? 'true' : 'false',
                 })
               }
-            >
-              <option value="false">Show active</option>
-              <option value="true">Show archived</option>
-            </select> */}
+            ></Select>
+          </div>
+
           <div className="flex-1">
             <Input
               type="text"
